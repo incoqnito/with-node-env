a1461cc59807882e213b2a336a620ada2a65a2d8
DragonRaider5 <marces.engel@googlemail.com>
2017-12-12 09:55:03 +0100

    Made runnable

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let platform = require('os').platform();
 let script = process.argv[process.argv.length - 1];
 let spawn = require('child_process').spawn;
 let child = spawn('npm', ['run', `${script}:${platform}`]);
+#!/usr/bin/env node
 
 child.stdout.on('data', data => console.log(`${data}`));
 child.stderr.on('data', data => console.log(`${data}`));
