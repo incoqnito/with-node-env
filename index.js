22b4ee488f935009a283f4522de0e00aa8dca19a
DragonRaider5 <marces.engel@googlemail.com>
2017-12-12 09:58:38 +0100

    Fixed logging

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const script = process.argv[process.argv.length - 1]
 const spawn = require('child_process').spawn
 const child = spawn('npm', ['run', `${script}:${platform}`])
 
-child.stdout.on('data', console.log)
-child.stderr.on('data', console.error)
+child.stdout.on('data', (buffer) => console.log(buffer.toString('utf8')))
+child.stderr.on('data', (buffer) => console.error(buffer.toString('utf8')))
